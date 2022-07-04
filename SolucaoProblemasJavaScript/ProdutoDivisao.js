10d754e605adb48c5f6d855ea851df4f838de703
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-07-04 09:38:07 -0300

    Create ProdutoDivisao.js

---
+++ SolucaoProblemasJavaScript/ProdutoDivisao.js
@@ -0,0 +1,19 @@
+const regex = /^([0-9]) *([*/])/;
+const imprimirResultado = (valorAcc) => console.log(typeof valorAcc == 'bigint' ? valorAcc : parseInt(valorAcc));
+const calcularDivisaoMultiplicacao = {
+    '/': (valorAcumulado, numero) => { return parseFloat(Number(valorAcumulado) / Number(numero)); },
+    '*': (valorAcumulado, numero) => { return Number(valorAcumulado) * Number(numero) >= Number.MAX_SAFE_INTEGER ? BigInt(valorAcumulado) * BigInt(numero) : valorAcumulado * numero; }
+};
+
+(function (entradaDeDados){
+  do {
+    let valorAcc = 1;
+    if (!entradaDeDados) break;
+
+    while (entradaDeDados--) {
+      const [, num, operador] = (gets()).match(regex);
+      valorAcc = calcularDivisaoMultiplicacao[operador](valorAcc, num);
+    }
+    imprimirResultado(valorAcc);    
+  } while (entradaDeDados = gets());
+})(gets());
